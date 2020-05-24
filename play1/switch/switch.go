cff6144fa8fbc27ad31f8d0d91254b2f9fd05bf0
mukulrawat1986 <mukulsrawat@gmail.com>
2020-05-24 22:22:23 +0530

    simple example of switch statement

---
+++ play1/switch/switch.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	n, err := fmt.Printf("Hello, World!\n")
+
+	switch {
+	case err != nil:
+		os.Exit(1)
+	case n == 0:
+		fmt.Printf("No bytes output\n")
+	case n != 14:
+		fmt.Printf("Wrong number of characters\n")
+	default:
+		fmt.Printf("Ok\n")
+	}
+
+	fmt.Printf("\n")
+}
