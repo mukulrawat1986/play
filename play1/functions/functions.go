8e3a4dfb4f11ba5853fa620d82616d243527020d
mukulrawat1986 <mukulsrawat@gmail.com>
2020-05-25 17:12:48 +0530

    add another parameter of different type

--- play1/functions/functions.go
+++ play1/functions/functions.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
-func printer(msg, msg2 string) {
-	fmt.Printf("%s", msg)
-	fmt.Printf("%s\n", msg2)
+func printer(msg, msg2 string, repeat int) {
+	for repeat > 0 {
+		fmt.Printf("%s", msg)
+		fmt.Printf("%s\n", msg2)
+		repeat--
+	}
 }
 
 func main() {
-	printer("Hello,", " World!")
+	printer("Hello,", " World!", 5)
 }
