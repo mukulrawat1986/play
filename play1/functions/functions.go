22aaeaa313de2997202edb7527fdee7f272d14b8
mukulrawat1986 <mukulsrawat@gmail.com>
2020-05-25 17:09:45 +0530

    multiple function parameters

--- play1/functions/functions.go
+++ play1/functions/functions.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func printer(msg string) {
-	fmt.Printf("%s\n", msg)
+func printer(msg, msg2 string) {
+	fmt.Printf("%s", msg)
+	fmt.Printf("%s\n", msg2)
 }
 
 func main() {
-	printer("Hello, World!")
+	printer("Hello,", " World!")
 }
