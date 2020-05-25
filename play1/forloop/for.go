083b0a3711e2e07b728573ea5f7200a093c5d997
mukulrawat1986 <mukulsrawat@gmail.com>
2020-05-25 17:02:09 +0530

    another example of for loop

--- play1/forloop/for.go
+++ play1/forloop/for.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func main() {
 
-	for i, j := 0, 1; i < 10; i, j = i+1, j*2 {
-		fmt.Printf("%d Hello, World\n", j)
+	var stop bool
+
+	i := 0
+
+	for !stop {
+		fmt.Printf("Hello, World\n")
+
+		i++
+		if i == 10 {
+			stop = true
+		}
 	}
 }
