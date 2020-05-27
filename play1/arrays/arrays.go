09e0718f4d1995916cb72cd5c16af18db3e1e7fc
mukulrawat1986 <mukulsrawat@gmail.com>
2020-05-27 22:51:53 +0530

    length and capacity of slices

--- play1/arrays/arrays.go
+++ play1/arrays/arrays.go
@@ -12,13 +12,19 @@ func printer(words []string) {
 
 func main() {
 
-	// creating an empty slice of length 4
-	words := make([]string, 4)
+	// creating an empty slice of length 0 and capacity 4
+	words := make([]string, 0, 4)
 
-	words[0] = "the"
-	words[1] = "quick"
-	words[2] = "brown"
-	words[3] = "fox"
-
+	fmt.Printf("%d %d\n", len(words), cap(words))
+	words = append(words, "the")
+	words = append(words, "quick")
+	words = append(words, "brown")
+	words = append(words, "fox")
+	fmt.Printf("%d %d\n", len(words), cap(words))
 	printer(words)
+	fmt.Printf("%d %d\n", len(words), cap(words))
+
+	newWords := make([]string, 4)
+	copy(newWords, words)
+	printer(newWords)
 }
